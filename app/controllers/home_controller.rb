abb803216e0db30beaded62869c1fc17de93a425
ygorpinto <ygor.pinto@hotmail.com>
2021-10-10 09:39:35 -0300

    ok

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
   def index
-    # swagger sei lá, se bem que como a api é interna, por enquanto não vai ter documentação
     render json: "Docs ..."
   end
 end
